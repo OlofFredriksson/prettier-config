e748195a2daea960402007905a1109c02b20751f
David Sveningsson <ext@sidvind.com>
2024-04-06 19:21:41 +0200

    fix: fix broken prettier cli binary

--- prettier.cjs
+++ prettier.cjs
@@ -4,7 +4,7 @@ const path = require("path");
 const { spawn } = require("child_process");
 
 const pkgPath = path.dirname(require.resolve("prettier/package.json"));
-const binary = path.join(pkgPath, "bin-prettier.js");
+const binary = path.join(pkgPath, "bin/prettier.cjs");
 
 spawn("node", [binary, ...process.argv.slice(2)], {
     stdio: "inherit",
